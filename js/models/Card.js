71c0f1e7985f6a6b741d4b7a979dcbbb82010ab0
Jeroen Breen <jeroen@innouveau.nl>
2016-10-13 13:04:55 +0200

    add ripple

--- js/models/Card.js
+++ js/models/Card.js
@@ -6,14 +6,15 @@ function Card(app, building) {
     this.hammer = null;
     this.create();
     this.addListener();
+    //this.float();
 }
 
 Card.prototype.create = function() {
     var content = this.building.getContent(),
         card = $('<div class="ventu-card">' +
-            '<div class="ventu-card-image ventu-triangle ventu-triangle-bottom ventu-triangle-light-grey" style="background-image:url(' + content.image + ')"></div>' +
+            '<div class="ventu-card-image ventu-triangle ventu-triangle-bottom ventu-triangle-white" style="background-image:url(' + content.image + ')"></div>' +
             '<div class="ventu-card-text">' + content.text.head + '</div>' +
-            '<div class="ventu-card-buttons"><div class="ventu-card-button"><div class="ventu-card-button-icon ventu-icon-hate"></div><span>niet interessant</span></div><div class="ventu-card-button"><div class="ventu-card-button-icon ventu-icon-details"></div><span>laat details zien</span></div><div class="ventu-card-button"><div class="ventu-card-button-icon ventu-icon-love"></div><span>interessant</span></div></div>' +
+            '<div class="ventu-card-buttons"><div class="ventu-card-button"><div class="ventu-card-button-icon ventu-icon-hate"><div class="ventu-ripple"></div></div><span>niet interessant</span></div><div class="ventu-card-button"><div class="ventu-card-button-icon ventu-icon-details"><div class="ventu-ripple"></div></div><span>laat details zien</span></div><div class="ventu-card-button"><div class="ventu-card-button-icon ventu-icon-love"><div class="ventu-ripple"></div></div><span>interessant</span></div></div>' +
             '<div class="ventu-card-icons"></div></div>'),
         shade = $('<div class="ventu-card-shade"></div>');
     this.app.domElements.container.append(card);
@@ -43,12 +44,11 @@ Card.prototype.addListener = function() {
             //self.domElements.suggest.css('opacity', 0);
 
             if (dx > self.app.config.swipe) {
-                self.love();
+                self.love(dx, dy);
             } else if (dx < -self.app.config.swipe) {
-                self.hate();
+                self.hate(dx, dy);
             } else {
                 if (dy > 200) {
-                    //self.love(); // JWA
                     self.seeDetail();
                 } else {
                     self.release();
@@ -58,15 +58,18 @@ Card.prototype.addListener = function() {
     });
 };
 
-Card.prototype.drag = function(dx, dy) {
-    var x = dx,
-        y = dy,
-        rotY = dx / 5,
-        rotX = dy / -5;
+Card.prototype.float = function() {
+    var self = this;
+    this.element.removeClass('no-transition');
+    this.element.addClass('ventu-card-float');
+    setTimeout(function(){
+        self._clearfloat();
+    }, 4000)
+};
+
+Card.prototype._clearfloat = function() {
     this.element.addClass('no-transition');
-    this.shade.addClass('no-transition');
-    this.setTransform(this.element, x,y,0,rotX,rotY,1,1);
-    this.setTransform(this.shade, 0.5*x,0.5*y + 100,-100,0,0,(1 - Math.abs(x/1000)), (1 - Math.abs(y/1000)));
+    this.element.removeClass('ventu-card-float');
 };
 
 Card.prototype.release = function() {
@@ -76,7 +79,31 @@ Card.prototype.release = function() {
     this.setTransform(this.shade,0,100,-100,0,0,1,1);
 };
 
-Card.prototype.love = function () {
+Card.prototype.drag = function(dx, dy) {
+    var x = dx,
+        y = dy,
+        rotY = dx / 5,
+        rotX = dy / -5,
+        rotZ = dx / 20;
+    this.element.addClass('no-transition');
+    this.shade.addClass('no-transition');
+    this.setTransform(this.element, x,y,0,rotX,rotY, rotZ, 1,1);
+    this.setTransform(this.shade, 0.5*x,0.5*y + 100,-100,0,0,0.5*rotZ,(1 - Math.abs(x/1000)), (1 - Math.abs(y/1000)));
+};
+
+Card.prototype.release = function() {
+    this.element.removeClass('no-transition');
+    this.shade.removeClass('no-transition');
+    this.setTransform(this.element,0,0,0,0,0,0,1,1);
+    this.setTransform(this.shade,0,100,-100,0,0,0,1,1);
+};
+
+Card.prototype.love = function (dx, dy) {
+    this.element.removeClass('no-transition');
+    this.shade.removeClass('no-transition');
+    this.setTransform(this.element,100,600,-200,0,0,0, 0.5,0.5);
+    this.setTransform(this.shade,100,550,-200,0,0,0, 0.5,0.5);
+
     // var currentObject = this.objects[this.currentObjectIndex];
     // if (currentObject) {
     //     this.app.service.post('LikeObject', currentObject.UniqueId);
@@ -96,7 +123,12 @@ Card.prototype.love = function () {
     // this.setCurrent();
 };
 
-Card.prototype.hate = function() {
+Card.prototype.hate = function(dx, dy) {
+    this.element.removeClass('no-transition');
+    this.shade.removeClass('no-transition');
+    this.setTransform(this.element,-100,600,-200,0,0,0, 0.5,0.5);
+    this.setTransform(this.shade,-100,550,-200,0,0,0, 0.5,0.5);
+
     // var currentObject = this.objects[this.currentObjectIndex];
     // if (currentObject) {
     //     this.app.service.post('DislikeObject', currentObject.UniqueId);
@@ -177,17 +209,18 @@ Card.prototype.moveCard = function(card, love) {
 
 
 
-Card.prototype._getTransform = function(x, y, z, rotX, rotY, scaleX, scaleY) {
+Card.prototype._getTransform = function(x, y, z, rotX, rotY, rotZ, scaleX, scaleY) {
     return 'translateZ(' + z + 'px) ' +
         'translateY(' + y + 'px) ' +
         'translateX(' + x + 'px) ' +
         'rotateX(' + rotX + 'deg) ' +
         'rotateY(' + rotY + 'deg) ' +
+        'rotateZ(' + rotZ + 'deg) ' +
         'scale(' + scaleX + ',' + scaleY + ')';
 };
 
-Card.prototype.setTransform = function(element, x, y, z, rotX, rotY, scaleX, scaleY) {
-    var transform = this._getTransform(x, y, z, rotX, rotY, scaleX, scaleY);
+Card.prototype.setTransform = function(element, x, y, z, rotX, rotY, rotZ, scaleX, scaleY) {
+    var transform = this._getTransform(x, y, z, rotX, rotY, rotZ, scaleX, scaleY);
     element.css({
         "webkitTransform": transform,
         "MozTransform": transform,
