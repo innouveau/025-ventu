79aa4a4ad5269e801016c159726337c8fa8636e4
Jeroen Breen <jeroen@innouveau.nl>
2017-05-11 23:45:01 +0200

    add home elements

--- js/home/main.js
+++ js/home/main.js
@@ -1,7 +1,12 @@
 $(window).ready(function(){
     initialiseModules();
     initialiseModals();
+
+    setFullscreen();
 });
 
 
-
+function setFullscreen() {
+    var windowHeight = $(window).outerHeight();
+    $('#ventu-dialog-entry').css('min-height', windowHeight-60);
+}
