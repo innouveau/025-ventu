27994217c1dd60b418e9e2684228d91ed14b1672
Jeroen Breen <jeroen@innouveau.nl>
2017-05-18 16:05:17 +0200

    full text animation

--- js/application/dom/filter-listeners.js
+++ js/application/dom/filter-listeners.js
@@ -1,10 +1,31 @@
 function filterListeners() {
-
     filterLabelListener();
+    filterSetFullTextLengths();
+    filterReadMoreListeners();
 }
 
 function filterLabelListener() {
     $('#ventu-filter-label').click(function(){
         $(this).parent().toggleClass('ventu-filters--inactive');
+    })
+}
+
+function filterSetFullTextLengths() {
+    $('.ventu-filter-full-text-container').each(function(){
+        var height = $(this).outerHeight();
+        $(this).attr('height', height).css('height', 0);
+    })
+}
+
+function filterReadMoreListeners() {
+    $('.ventu-filter-read-more').click(function(){
+        var fullText = $(this).parent().parent().find('.ventu-filter-full-text-container');
+        $(this).toggleClass('ventu-filter-read-more--open');
+
+        if ($(this).hasClass('ventu-filter-read-more--open')) {
+            fullText.css('height', parseInt(fullText.attr('height')));
+        } else {
+            fullText.css('height', 0);
+        }
     })
 }
