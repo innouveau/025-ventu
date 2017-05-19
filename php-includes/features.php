74864eb4f74f1d1a0d00539d6064f43f706bae77
Jeroen Breen <jeroen@innouveau.nl>
2017-05-19 15:40:58 +0200

    use filter content id's in css

--- php-includes/features.php
+++ php-includes/features.php
@@ -1,6 +1,6 @@
 <div class="ventu-features">
     <ul>
-        <li class="ventu-features-shop">
+        <li class="ventu-features-objectusage-3">
             Winkel
         </li>
         <li class="ventu-features-location">
@@ -12,7 +12,7 @@
         <li class="ventu-features-price">
             Huurprijs p/j € 72.000, Huurprijs p/m € 6.000
         </li>
-        <li class="ventu-features-office">
+        <li class="ventu-features-objectusage-1">
             Inclusief kantoorruimte
         </li>
     </ul>
