244479b77566daad2842d2dd38f50b41dbd49abd
Jeroen Breen <jeroen@innouveau.nl>
2017-05-19 16:20:40 +0200

    card variantions

---
+++ cards.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ventu Cards</title>
+
+    <?php include('php-includes/standard-files.php'); ?>
+
+    <?php include('php-includes/application-links.php'); ?>
+
+    <script src="js/home/main.js"></script>
+</head>
+
+<body class="ventu-home">
+
+
+<?php include('php-includes/nav.php'); ?>
+
+
+<div class="container">
+    <section>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="ventu-card ventu-card--static ventu-card--grid">
+                    <div class="ventu-card-image" style="background-image:url(img/content/kantoor2.jpg)"></div>
+                    <div class="ventu-card-text">
+                        <div class="ventu-card-header">
+                            <h4>Test Cards</h4>
+                            <h3>Grid Variant</h3>
+                        </div>
+                        <?php include('php-includes/features.php');?>
+                    </div>
+                    <?php include('php-includes/card/buttons.php');?>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="ventu-card ventu-card--static ventu-card--grid">
+                    <div class="ventu-card-image" style="background-image:url(img/content/kantoor2.jpg)"></div>
+                    <div class="ventu-card-text">
+                        <div class="ventu-card-header">
+                            <h4>Test Cards</h4>
+                            <h3>Grid Variant 1 button (group)</h3>
+                        </div>
+                        <?php include('php-includes/features.php');?>
+                    </div>
+                    <div class="ventu-card-buttons">
+                        <div class="ventu-card-button-container ventu-card-button-container--full">
+                            <div class="ventu-card-button ventu-card-button--group">
+                                <div class="ventu-card-button-icon"></div>
+                            </div>
+                            <div class="ventu-card-button-label">
+                                Laat groep zien
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="ventu-card ventu-card--absolute" style="-moz-user-select: none; touch-action: none; transform: translateX(50px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(-8deg) scale(1, 1);">
+                    <div class="ventu-card-image" style="background-image:url(img/content/kantoor2.jpg)"></div>
+                    <div class="ventu-card-text">
+                        <div class="ventu-card-header">
+                            <h4>Test Cards</h4>
+                            <h3>Absolute Variant</h3>
+                        </div>
+                        <?php include('php-includes/features.php');?>
+                    </div>
+                    <?php include('php-includes/card/buttons.php');?>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="ventu-card ventu-card--grid ventu-card--float">
+                    <div class="ventu-card-image" style="background-image:url(img/content/kantoor2.jpg)"></div>
+                    <div class="ventu-card-text">
+                        <div class="ventu-card-header">
+                            <h4>Test Cards 1</h4>
+                            <h3>Float variant</h3>
+                        </div>
+                        <?php include('php-includes/features.php');?>
+                    </div>
+                    <?php include('php-includes/card/buttons.php');?>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="ventu-card ventu-card--grid ventu-card--float">
+                    <div class="ventu-card-image" style="background-image:url(img/content/kantoor2.jpg)"></div>
+                    <div class="ventu-card-text">
+                        <div class="ventu-card-header">
+                            <h4>Test Cards 2</h4>
+                            <h3>Float Variant</h3>
+                        </div>
+                        <?php include('php-includes/features.php');?>
+                    </div>
+                    <?php include('php-includes/card/buttons.php');?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+</div>
+
+<?php include('php-includes/modals.php'); ?>
+</body>
+</html>
---
+++ php-includes/card/buttons.php
@@ -0,0 +1,18 @@
+<div class="ventu-card-buttons">
+    <div class="ventu-card-button-container">
+        <div class="ventu-card-button ventu-card-button--hate">
+            <div class="ventu-card-button-icon"></div>
+        </div>
+        <div class="ventu-card-button-label">
+            Niet interessant
+        </div>
+    </div>
+    <div class="ventu-card-button-container">
+        <div class="ventu-card-button ventu-card-button--share">
+            <div class="ventu-card-button-icon"></div>
+        </div>
+        <div class="ventu-card-button-label">
+            Zet in gedeeld
+        </div>
+    </div>
+</div>
