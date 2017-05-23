fd60302f655fb123ce06b9d3c23a1218548da92f
Jeroen Breen <jeroen@innouveau.nl>
2017-05-23 18:19:22 +0200

    add filter button icons

--- php-includes/application/filters.php
+++ php-includes/application/filters.php
@@ -2,152 +2,164 @@
     <div class="ventu-slide-panel-label"></div>
 
     <div class="ventu-slide-panel-content">
-        <div class="ventu-filter-section">
-            <h5>
-                <span id="ventu-filter-result">329</span> <span>objecten gevonden</span>
-            </h5>
-        </div>
 
-        <!-- location -->
-        <div id="ventu-filter-location" class="ventu-filter-section">
-            <div class="ventu-filter-header">
-                Zoekopdracht:
+        <div id="ventu-filter-content">
+            <div class="ventu-filter-section">
+                <h5>
+                    <span id="ventu-filter-result">329</span> <span>objecten gevonden</span>
+                </h5>
             </div>
-            <div class="ventu-filter-label-container">
-                <div class="ventu-filter-label">
-                    Amsterdam
+
+            <!-- location -->
+            <div id="ventu-filter-location" class="ventu-filter-section">
+                <div class="ventu-filter-header">
+                    Zoekopdracht:
                 </div>
-            </div>
-
-            <div class="ventu-filter-full-text-container">
-                <div class="ventu-filter-full-text">
-
+                <div class="ventu-filter-label-container">
+                    <div class="ventu-filter-label">
+                        Amsterdam
+                    </div>
                 </div>
-            </div>
-        </div>
 
-        <!-- types -->
-        <div id="ventu-filter-types" class="ventu-filter-section">
-            <div class="ventu-filter-header">
-                Type object:
-                <div class="ventu-filter-read-more">
-                    <div class="ventu-filter-read-more-label">
-                        <span class="ventu-filter-read-more-label--close">Meer</span>
-                        <span class="ventu-filter-read-more-label--open">Minder</span>
+                <div class="ventu-filter-full-text-container">
+                    <div class="ventu-filter-full-text">
+
                     </div>
                 </div>
             </div>
-            <div class="ventu-filter-label-container">
-                <div class="ventu-filter-label">
-                    Winkel
-                </div>
-                <div class="ventu-filter-label">
-                    Kantoor
-                </div>
-            </div>
 
-            <div class="ventu-filter-full-text-container">
-                <div class="ventu-filter-full-text">
-                    <div id="ventu-filter-types-buttons"><!-- appended dynamically --></div>
-
+            <!-- types -->
+            <div id="ventu-filter-types" class="ventu-filter-section">
+                <div class="ventu-filter-header">
+                    Type object:
+                    <div class="ventu-filter-read-more">
+                        <div class="ventu-filter-read-more-label">
+                            <span class="ventu-filter-read-more-label--close">Meer</span>
+                            <span class="ventu-filter-read-more-label--open">Minder</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+                <div class="ventu-filter-label-container">
+                    <div class="ventu-filter-label">
+                        Winkel
+                    </div>
+                    <div class="ventu-filter-label">
+                        Kantoor
+                    </div>
+                </div>
 
-        <!-- area -->
-        <div id="ventu-filter-area" class="ventu-filter-section">
-            <div class="ventu-filter-header">
-                Oppervlakte:
-                <div class="ventu-filter-read-more">
-                    <div class="ventu-filter-read-more-label">
-                        <span class="ventu-filter-read-more-label--close">Meer</span>
-                        <span class="ventu-filter-read-more-label--open">Minder</span>
+                <div class="ventu-filter-full-text-container">
+                    <div class="ventu-filter-full-text">
+                        <div id="ventu-filter-types-buttons"><!-- appended dynamically --></div>
+
                     </div>
                 </div>
             </div>
-            <div class="ventu-filter-label-container">
-                van
-                <div id="ventu-filter-area-min" class="ventu-filter-label">
-                    140 m²
-                </div>
-                tot
-                <div id="ventu-filter-area-max" class="ventu-filter-label">
-                    250 m²
-                </div>
-            </div>
 
-            <div class="ventu-filter-full-text-container">
-                <div class="ventu-filter-full-text">
+            <!-- area -->
+            <div id="ventu-filter-area" class="ventu-filter-section">
+                <div class="ventu-filter-header">
+                    Oppervlakte:
+                    <div class="ventu-filter-read-more">
+                        <div class="ventu-filter-read-more-label">
+                            <span class="ventu-filter-read-more-label--close">Meer</span>
+                            <span class="ventu-filter-read-more-label--open">Minder</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="ventu-filter-label-container">
                     van
-                    <div class="ventu-filter-area-box">
-                        min
-                        <input id="ventu-filter-area-min-input">
-                        m²
+                    <div id="ventu-filter-area-min" class="ventu-filter-label">
+                        140 m²
                     </div>
                     tot
-                    <div class="ventu-filter-area-box">
-                        max
-                        <input id="ventu-filter-area-max-input">
-                        m²
+                    <div id="ventu-filter-area-max" class="ventu-filter-label">
+                        250 m²
+                    </div>
+                </div>
+
+                <div class="ventu-filter-full-text-container">
+                    <div class="ventu-filter-full-text">
+                        van
+                        <div class="ventu-filter-area-box">
+                            min
+                            <input id="ventu-filter-area-min-input">
+                            m²
+                        </div>
+                        tot
+                        <div class="ventu-filter-area-box">
+                            max
+                            <input id="ventu-filter-area-max-input">
+                            m²
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- transaction -->
+            <div id="ventu-filter-transaction" class="ventu-filter-section">
+                <div class="ventu-filter-header">
+                    Aanbod:
+                    <div class="ventu-filter-read-more">
+                        <div class="ventu-filter-read-more-label">
+                            <span class="ventu-filter-read-more-label--close">Meer</span>
+                            <span class="ventu-filter-read-more-label--open">Minder</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="ventu-filter-label-container">
+                    <div class="ventu-filter-label">
+                        Huren
+                    </div>
+                </div>
+
+                <div class="ventu-filter-full-text-container">
+                    <div class="ventu-filter-full-text">
+                        <div id="ventu-filter-transaction-buttons"><!-- appended dynamically --></div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- search-area -->
+            <div id="ventu-filter-search-area" class="ventu-filter-section">
+                <div class="ventu-filter-header">
+                    Aangepast zoekgebied:
+                    <div class="ventu-filter-read-more">
+                        <div class="ventu-filter-read-more-label">
+                            <span class="ventu-filter-read-more-label--close">Meer</span>
+                            <span class="ventu-filter-read-more-label--open">Minder</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="ventu-filter-label-container">
+                    <div class="ventu-filter-passive-text">
+                        Niet actief
+                    </div>
+                </div>
+
+                <div class="ventu-filter-full-text-container">
+                    <div class="ventu-filter-full-text">
+
                     </div>
                 </div>
             </div>
         </div>
 
-        <!-- transaction -->
-        <div id="ventu-filter-transaction" class="ventu-filter-section">
-            <div class="ventu-filter-header">
-                Aanbod:
-                <div class="ventu-filter-read-more">
-                    <div class="ventu-filter-read-more-label">
-                        <span class="ventu-filter-read-more-label--close">Meer</span>
-                        <span class="ventu-filter-read-more-label--open">Minder</span>
-                    </div>
-                </div>
-            </div>
-            <div class="ventu-filter-label-container">
-                <div class="ventu-filter-label">
-                    Huren
-                </div>
-            </div>
 
-            <div class="ventu-filter-full-text-container">
-                <div class="ventu-filter-full-text">
-                    <div id="ventu-filter-transaction-buttons"><!-- appended dynamically --></div>
-                </div>
-            </div>
-        </div>
-
-        <!-- search-area -->
-        <div id="ventu-filter-search-area" class="ventu-filter-section">
-            <div class="ventu-filter-header">
-                Aangepast zoekgebied:
-                <div class="ventu-filter-read-more">
-                    <div class="ventu-filter-read-more-label">
-                        <span class="ventu-filter-read-more-label--close">Meer</span>
-                        <span class="ventu-filter-read-more-label--open">Minder</span>
-                    </div>
-                </div>
-            </div>
-            <div class="ventu-filter-label-container">
-                <div class="ventu-filter-passive-text">
-                    Niet actief
-                </div>
-            </div>
-
-            <div class="ventu-filter-full-text-container">
-                <div class="ventu-filter-full-text">
-
-                </div>
-            </div>
-        </div>
 
 
         <!-- tools -->
-        <div id="ventu-filter-tools" class="ventu-filter-section">
-            <div class="ventu-filter-tools-button ventu-filter-tools-button--save">
-                <div class="ventu-filter-tools-button-label">
+        <div id="ventu-filter-tools">
+            <div class="ventu-tools-button ventu-tools-button--save">
+                <div class="ventu-tools-button-icon"></div>
+                <div class="ventu-tools-button-label">
                     Sla deze zoekopdracht op
+                </div>
+            </div>
+            <div class="ventu-tools-button ventu-tools-button--erase">
+                <div class="ventu-tools-button-icon"></div>
+                <div class="ventu-tools-button-label">
+                    Wis alle filters
                 </div>
             </div>
         </div>
