b7eb6d0da6d83aaa67b453a00effa00d03270897
Jeroen Breen <jeroen@innouveau.nl>
2017-05-23 19:10:16 +0200

    add hangout icons

--- detail.php
+++ detail.php
@@ -331,67 +331,7 @@ indruk van het woon en/of werkgebied.</p>
                 </div>
             </section>
 
-            <section class="parties clearfix section--with-shadow section--small-padding">
-                <h5>Contact</h5>
-                <div class="divider"></div>
-
-                    <div class="margin-left-20" itemscope="" itemtype="http://schema.org/RealEstateAgent">
-                        <div itemscope="" itemtype="http://schema.org/Organization" class="clearfix">
-                                <img itemprop="logo" class="broker-logo" src="img/temp_detail/1.png" alt="Logo CBRE">
-
-                            <div class="row">
-                                <div class="col-xs-1">
-                                    <img src="img/temp_detail/profile.svg">
-                                </div>
-                                <div class="col-xs-11">
-                                    <h5>Aanbieder</h5>
-                                    <p itemprop="legalName">CBRE</p>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-xs-1">
-                                    <img src="img/temp_detail/react.svg">
-                                </div>
-                                <div class="col-xs-11">
-                                    <h5>Contactpersoon</h5>
-                                    <p itemprop="name">Eveline Davidis</p>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-xs-1">
-                                    <img src="img/temp_detail/phone.svg">
-                                </div>
-                                <div class="col-xs-11">
-                                    <h5>Telefoon</h5>
-                                    <div class="show-phonenumber">
-                                        <p><a href="javascript:void(0);" data-event="ShowPhoneNumber">Toon telefoonnummer</a></p>
-                                        <div class="hidden">
-                                            <p itemprop="telephone">020-6262691</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-xs-1">
-                                    <img src="img/temp_detail/email.svg">
-                                </div>
-                                <div class="col-xs-11">
-                                    <h5>E-mailadres</h5>
-                                    <div class="show-emailaddress">
-                                        <p><a href="javascript:void(0);" data-event="ShowEmailAddress">Toon e-mailadres</a></p>
-                                        <div class="hidden">
-                                            <p itemprop="email">eveline.davidis@cbre.com</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-            </section>
+            <?php include('php-includes/parties.php'); ?>
         </div>
     </div>
 
---
+++ php-includes/parties.php
@@ -0,0 +1,69 @@
+<section id="ventu-parties" class="clearfix section--with-shadow section--small-padding">
+    <h5>Contact</h5>
+    <div class="divider"></div>
+
+    <div class="margin-left-20" itemscope="" itemtype="http://schema.org/RealEstateAgent">
+        <div itemscope="" itemtype="http://schema.org/Organization" class="clearfix">
+            <img itemprop="logo" class="broker-logo" src="img/temp_detail/1.png" alt="Logo CBRE">
+
+            <div class="row">
+                <div class="col-xs-2">
+                    <div class="ventu-parties-contact-icon">
+                        <img src="img/icons/contact/profile-grey.svg">
+                    </div>
+                </div>
+                <div class="col-xs-10">
+                    <h5>Aanbieder</h5>
+                    <p itemprop="legalName">CBRE</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-2">
+                    <div class="ventu-parties-contact-icon">
+                        <img src="img/icons/contact/react-grey.svg">
+                    </div>
+                </div>
+                <div class="col-xs-10">
+                    <h5>Contactpersoon</h5>
+                    <p itemprop="name">Eveline Davidis</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-2">
+                    <div class="ventu-parties-contact-icon">
+                        <img src="img/icons/contact/phone-grey.svg">
+                    </div>
+                </div>
+                <div class="col-xs-10">
+                    <h5>Telefoon</h5>
+                    <div class="show-phonenumber">
+                        <p><a href="javascript:void(0);" data-event="ShowPhoneNumber">Toon telefoonnummer</a></p>
+                        <div class="hidden">
+                            <p itemprop="telephone">020-6262691</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-2">
+                    <div class="ventu-parties-contact-icon">
+                        <img src="img/icons/contact/email-grey.svg">
+                    </div>
+                </div>
+                <div class="col-xs-10">
+                    <h5>E-mailadres</h5>
+                    <div class="show-emailaddress">
+                        <p><a href="javascript:void(0);" data-event="ShowEmailAddress">Toon e-mailadres</a></p>
+                        <div class="hidden">
+                            <p itemprop="email">eveline.davidis@cbre.com</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
