50e2d9d489dad2d6143273ce2020b95ea4890280
Jeroen Breen <jeroen@innouveau.nl>
2017-05-24 08:32:31 +0200

    add empty profile avatar

--- hangout-mijn-lijsten.php
+++ hangout-mijn-lijsten.php
@@ -44,6 +44,7 @@
                 <div class="hangout-list-entry-shared-with">
                     <div class="hangout-list-entry-shared-with-number">+3</div>
                     <div class="ventu-avatar-image" style="background-image:url(img/content/4c299dd05d3c6fd7208e18dbd7d824c4_400x400.jpeg)"></div>
+                    <div class="ventu-avatar-image ventu-avatar-image--empty-profile"></div>
                     <div class="ventu-avatar-image ventu-avatar-image--no-avatar"></div>
                     <div class="ventu-avatar-image" style="background-image:url(img/content/gp5xqleg0f8jbkxuazos.jpeg)"></div>
                 </div>
@@ -66,6 +67,7 @@
                 <div class="hangout-list-entry-shared-with">
                     <div class="hangout-list-entry-shared-with-number">+3</div>
                     <div class="ventu-avatar-image" style="background-image:url(img/content/4c299dd05d3c6fd7208e18dbd7d824c4_400x400.jpeg)"></div>
+                    <div class="ventu-avatar-image ventu-avatar-image--empty-profile"></div>
                     <div class="ventu-avatar-image ventu-avatar-image--no-avatar"></div>
                     <div class="ventu-avatar-image" style="background-image:url(img/content/gp5xqleg0f8jbkxuazos.jpeg)"></div>
                 </div>
