bd50f8c5830b081d5347b0ae52d23498e3528ce0
Jeroen Breen <jeroen@innouveau.nl>
2017-05-25 22:14:53 +0200

    fix home card buttons

--- php-includes/search-entry.php
+++ php-includes/search-entry.php
@@ -21,29 +21,37 @@
         <div class="col-sm-6">
             <div id="ventu-home-card-container">
                 <!-- ventu card -->
-                <div class="ventu-card ventu-card--static"
+                <div class="ventu-card ventu-card--absolute"
                      style="-moz-user-select: none; touch-action: none; transform: translateX(50px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(-3deg) scale(1, 1); display: block;">
-                    <div class="ventu-card-image" style="background-image:url(https://ventu.nl/media/objects/d0/15/8f/fe/d0158ffe-178d-41d7-a8af-8117df71f5bf/thumb/1.jpg)"></div>
+                    <div class="ventu-card-image">
+                        <div class="aspect16by9">
+                            <img src="img/content/kantoor2.jpg">
+                        </div>
+                    </div>
                     <div class="ventu-card-text">
                         <div class="ventu-card-header">
-                            <h4>J.C. van Markenlaan 3, 2285 VL, Rijswijk</h4>
-                            <h3>Moerdijk</h3>
+                            <h4>Test Cards</h4>
+                            <h3>Grid Variant - 2 buttons</h3>
                         </div>
-                        <?php include('features.php');?>
+                        <?php include('php-includes/features.php');?>
                     </div>
                 </div>
                 <!-- end of card -->
 
                 <!-- ventu card -->
-                <div class="ventu-card ventu-card--static"
+                <div class="ventu-card ventu-card--absolute"
                      style="-moz-user-select: none; touch-action: none; transform: translateX(180px) translateY(40px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(6deg) scale(1, 1); display: block;">
-                    <div class="ventu-card-image" style="background-image:url(https://ventu.nl/media/objects/13/d7/4c/f8/13d74cf8-8bc6-43d2-ae10-7d9319cbc5a9/thumb/1.jpg)"></div>
+                    <div class="ventu-card-image">
+                        <div class="aspect16by9">
+                            <img src="img/content/kantoor2.jpg">
+                        </div>
+                    </div>
                     <div class="ventu-card-text">
                         <div class="ventu-card-header">
-                            <h4>J.C. van Markenlaan 3, 2285 VL, Rijswijk</h4>
-                            <h3>Moerdijk</h3>
+                            <h4>Test Cards</h4>
+                            <h3>Grid Variant - 2 buttons</h3>
                         </div>
-                        <?php include('features.php');?>
+                        <?php include('php-includes/features.php');?>
                     </div>
                 </div>
                 <!-- end of card -->
@@ -53,17 +61,23 @@
 
     <div class="row">
         <div class="col-sm-12">
-            <div id="ventu-card-button-container">
-                <div class="ventu-card-button ventu-card-button--love">
-                    <div class="ventu-card-button-icon"></div>
+            <div id="ventu-home-card-button-container">
+                <div class="ventu-card-button-container ventu-card-button--love">
+                    <div class="ventu-card-button">
+                        <div class="ventu-card-button-icon"></div>
+                    </div>
                 </div>
 
-                <div class="ventu-card-button ventu-card-button--hate">
-                    <div class="ventu-card-button-icon"></div>
+                <div class="ventu-card-button-container ventu-card-button--hate">
+                    <div class="ventu-card-button">
+                        <div class="ventu-card-button-icon"></div>
+                    </div>
                 </div>
 
-                <div class="ventu-card-button ventu-card-button--read-more">
-                    <div class="ventu-card-button-icon"></div>
+                <div class="ventu-card-button-container ventu-card-button--read-more">
+                    <div class="ventu-card-button">
+                        <div class="ventu-card-button-icon"></div>
+                    </div>
                 </div>
             </div>
         </div>
