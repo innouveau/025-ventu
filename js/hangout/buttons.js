955a1dfe48213ddca6f23864fcd69572e3bcc83d
j.walstra@forward-solutions.nl <@4631Mb2>
2017-05-27 07:23:38 +0200

    Removed log

--- js/hangout/buttons.js
+++ js/hangout/buttons.js
@@ -3,7 +3,7 @@ $(window).ready(function(){
     $('.ventu-hangout-tip').each(function(){
         var hangoutTip = $(this),
             closeButton = $('<div class="ventu-hangout-tip-close"></div>');
-        console.log(hangoutTip);
+        // console.log(hangoutTip);
         hangoutTip.append(closeButton);
 
         closeButton.click(function(){
