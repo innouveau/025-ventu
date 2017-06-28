120a8597c7e666b049941169d6c0e5535bde07a1
Jeroen Breen <jeroen@innouveau.nl>
2017-06-28 17:44:05 +0200

    create chatbox #13

---
+++ hangout-chat.php
@@ -0,0 +1,130 @@
+<?php include('php-includes/static-card.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ventu Hangout Mijn Lijsten</title>
+
+    <?php include('php-includes/standard-files.php'); ?>
+
+    <script src="js/hangout/buttons.js"></script>
+
+</head>
+
+<body class="ventu-hangout">
+
+<?php include('php-includes/nav-logged-in.php'); ?>
+
+
+<div class="container">
+    <section id="ventu-hangout-header-back" class="ventu-hangout-header--share">
+        <div class="row">
+            <div class="col-sm-4">
+                <a class="ventu-hangout-header-back-button" href="">
+                    Terug
+                </a>
+            </div>
+            <div class="col-sm-8">
+                <div class="ventu-hangout-list-members">
+                    <?php include('php-includes/hangout/members.php'); ?>
+                    <div class="ventu-hangout-list-members-label">
+                        Mijn teamleden:
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="ventu-hangout-header-title" class="ventu-hangout-header--share">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>
+                    Dit is onze samen-zoeken lijst
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <?php include('php-includes/hangout/tools.php'); ?>
+            </div>
+        </div>
+    </section>
+
+    <section id="ventu-hangout-content-section">
+        <div class="ventu-hangout-lists-wrapper">
+
+            <div class="ventu-hangout-tip ventu-hangout-tip-content">
+                <h5>
+                    <span class="ventu-hangout-tip-button ventu-hangout-tip-button--profile"></span>
+                    Samen vinden doe je met Ventu!
+                </h5>
+                <p>
+                    Samen vinden doe je met Ventu. Daarom kan je eenvoudig op Ventu lijstjes maken en delen met anderen. Gebruik het <span class="ventu-icon ventu-icon--in-text ventu-icon--add"></span> icoon om nieuwe objecten toe te voegen en nodig jouw collega’s per email uit om te reageren en stemmen op hun favoriete objecten.
+                </p>
+            </div>
+
+
+
+            <div class="ventu-card ventu-card--grid ventu-card--float">
+                <div class="ventu-card-image">
+                    <div class="aspect16by9">
+                        <a href="http://realspotter.nl/Project/Nieuwegein/Fultonbaan/16/1E87D177-2066-4047-8114-F25EEF3FBB01?c=1"><img src="http://realspotter.nl/media/objects/1E/87/D1/77/1E87D177-2066-4047-8114-F25EEF3FBB01/org/1.jpg"></a>
+                    </div>
+                </div>
+
+                <div class="ventu-card-text">
+                    <div class="ventu-card-header">
+                        <h4>Plettenburgh, Fultonbaan  16, 3439 NE, Nieuwegein</h4>
+                    </div>
+                    <div class="ventu-features">
+                        <ul>
+                            <li class="ventu-features-objectusage-1">
+                                Kantoor
+                            </li>
+                            <li class="ventu-features-location">
+                                Huur
+                            </li>
+                            <li class="ventu-features-metrage">
+                                1.115 m&sup2;
+                            </li>
+                            <li class="ventu-features-price">
+                                Huurprijs: &euro; 95 m<sup>2</sup>/jr
+                            </li>
+
+                        </ul>
+                    </div>
+                    <?php include('php-includes/card/rating.php'); ?>
+                </div>
+
+
+                <div class="ventu-card-buttons ventu-card-buttons-1">
+                    <div class="ventu-card-button-container ventu-card-button--hate">
+                        <div class="ventu-card-button">
+                            <div class="ventu-card-button-icon"></div>
+                        </div>
+                        <div class="ventu-card-button-label">
+                            Verplaats object naar de niet interessant lijst
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <?php include('php-includes/about.php'); ?>
+
+    <?php include('php-includes/footer.php'); ?>
+
+    <?php include('php-includes/modals.php'); ?>
+
+    <?php include('php-includes/hangout/chat.php'); ?>
+</div>
+
+
+</body>
+</html>
---
+++ php-includes/hangout/chat.php
@@ -0,0 +1,85 @@
+<div class="ventu-overlay" style="display: block;">
+    <div class="ventu-teamchat-popup-close ventu-popup-close"> </div>
+
+
+    <div class="ventu-popup--hangout-chat ventu-popup" style="display: block;">
+        <div class="ventu-popup-body">
+            <img class="ventu-hangout-chat-image" src="http://realspotter.nl/media/objects/22/05/36/ff/220536ff-c1a8-469f-83e0-af6c1a415adc/thumb/1.jpg">
+
+            <div class="ventu-feedback-container ventu-feedback-container-yours">
+                <div class="ventu-hangout-member-avatar">
+                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                </div>
+                <div class="ventu-chat-balloon-container">
+                    <div class="ventu-chat-balloon">
+                        <div class="ventu-chat-balloon-top"></div>
+                        <div class="ventu-chat-balloon-middle">
+                            <p class="ventu-member-name">Jan Willem H. de Graaf</p>
+                            <p class="ventu-member-feedback">hij lijkt het nu te doen</p>
+                            <p class="ventu-member-date">14 juni</p>
+                        </div>
+                        <div class="ventu-chat-balloon-bottom"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ventu-feedback-container">
+                <div class="ventu-hangout-member-avatar">
+                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
+                </div>
+                <div class="ventu-chat-balloon-container">
+                    <div class="ventu-chat-balloon">
+                        <div class="ventu-chat-balloon-top"></div>
+                        <div class="ventu-chat-balloon-middle">
+                            <p class="ventu-member-name">Robert</p>
+                            <p class="ventu-member-feedback">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec enim eget ante tristique rhoncus sed eu ante. Nam placerat risus vel tortor </p>
+                            <p class="ventu-member-date">14 juni</p>
+                        </div>
+                        <div class="ventu-chat-balloon-bottom"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ventu-feedback-container">
+                <div class="ventu-hangout-member-avatar">
+                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
+                </div>
+                <div class="ventu-chat-balloon-container">
+                    <div class="ventu-chat-balloon">
+                        <div class="ventu-chat-balloon-top"></div>
+                        <div class="ventu-chat-balloon-middle">
+                            <p class="ventu-member-name">Robert</p>
+                            <p class="ventu-member-feedback">hmmm... ik weet het nog niet..</p>
+                            <p class="ventu-member-date">14 juni</p>
+                        </div>
+                        <div class="ventu-chat-balloon-bottom"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ventu-feedback-container ventu-feedback-container-yours">
+                <div class="ventu-hangout-member-avatar">
+                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                </div>
+                <div class="ventu-chat-balloon-container">
+                    <div class="ventu-chat-balloon">
+                        <div class="ventu-chat-balloon-top"></div>
+                        <div class="ventu-chat-balloon-middle">
+                            <p class="ventu-member-name">Jan Willem H. de Graaf</p>
+                            <p class="ventu-member-feedback">:)</p>
+                            <p class="ventu-member-date">14 juni</p>
+                        </div>
+                        <div class="ventu-chat-balloon-bottom"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ventu-hangout-chat-input">
+            <form class="teamFeedback" novalidate="novalidate">
+                <input id="teamFeedback" name="teamFeedback" placeholder="Schrijf een reactie..." required="" aria-required="true" type="text">
+                <a id="btn-add-stack-feedback"></a>
+            </form>
+        </div>
+    </div>
+</div>
