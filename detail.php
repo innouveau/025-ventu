79e80a430fda21eb69eda0348603332a3d7b9553
Jeroen Breen <jeroen@innouveau.nl>
2017-07-03 17:19:54 +0200

    move stuff to php

--- detail.php
+++ detail.php
@@ -221,25 +221,7 @@ indruk van het woon en/of werkgebied.</p>
 
             <?php include('php-includes/teams.php'); ?>
 
-            <section id="ventu-features" class="clearfix section--with-shadow section--small-padding">
-                <h5>Kenmerken</h5>
-                <div class="divider"></div>
-                <ol>
-                    <li>Adres: Herengracht  477</li>
-                    <li>Plaats: Amsterdam</li>
-                    <li>Gepubliceerd op: 17-01-2017</li>
-                    <li>Gebouw: Kantoor</li>
-                    <li>Status: Gereed</li>
-                    <li>Beschikbaarheid: Beschikbaar per direct</li>
-                    <li>Oppervlakte: 200 m² </li>
-                    <li>Oppervlakte (bruto verhuurbaar): 1807 m²</li>
-                    <li>Aantal keren bezocht: <span id="ObjectStatistics">10498</span></li>
-                </ol>
-                <div class="divider"></div>
-                <a href="javascript:void(0);" class="btn orange">
-                    Download de brochure
-                </a>
-            </section>
+            <?php include('php-includes/detail-features.php'); ?>
 
             <section id="ventu-social" class="clearfix section--without-side-padding section--transparent">
                 <div class="row">
---
+++ php-includes/detail-features.php
@@ -0,0 +1,19 @@
+<section id="ventu-features" class="clearfix section--with-shadow section--small-padding">
+    <h5>Kenmerken</h5>
+    <div class="divider"></div>
+    <ol>
+        <li>Adres: Herengracht  477</li>
+        <li>Plaats: Amsterdam</li>
+        <li>Gepubliceerd op: 17-01-2017</li>
+        <li>Gebouw: Kantoor</li>
+        <li>Status: Gereed</li>
+        <li>Beschikbaarheid: Beschikbaar per direct</li>
+        <li>Oppervlakte: 200 m² </li>
+        <li>Oppervlakte (bruto verhuurbaar): 1807 m²</li>
+        <li>Aantal keren bezocht: <span id="ObjectStatistics">10498</span></li>
+    </ol>
+    <div class="divider"></div>
+    <a href="javascript:void(0);" class="btn orange">
+        Download de brochure
+    </a>
+</section>
