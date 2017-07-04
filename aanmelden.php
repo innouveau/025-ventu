b0ccace61c5bb838f4fa85bb3b0c27d772f0d249
Jeroen Breen <jeroen@innouveau.nl>
2017-07-04 23:16:36 +0200

    html account page

--- aanmelden.php
+++ aanmelden.php
@@ -16,45 +16,58 @@
 
         <?php include('php-includes/nav.php'); ?>
 
-        <section class="ventu-sign-in">
+        <section id="ventu-hangout-header-back">
+            <div class="row">
+                <div class="col-sm-12">
+                    <a class="ventu-hangout-header-back-button" href="javascript: window.history.back();">
+                        Terug
+                    </a>
+                </div>
+            </div>
+        </section>
 
-                <h2>Aanmelden</h2>
-                <div class="ventu-communication-screen">
-                    <div class="ventu-sign-in-container">
-                        <div class="ventu-man-container">
-                            <div class="ventu-man">
-                                <div class="ventu-man-icon ventu-man-icon--account"></div>
-                                <div class="ventu-chat-balloon-container ventu-chat-balloon-left">
-                                    <div class="ventu-chat-balloon">
-                                        <div class="ventu-chat-balloon-top"></div>
-                                        <div class="ventu-chat-balloon-middle">
-                                            We zullen jouw gegevens niet delen met externe partijen…
-                                            lorem ipsum dolor sit amet.
-                                        </div>
-                                        <div class="ventu-chat-balloon-bottom"></div>
+        <section class="header-section">
+            <h1>
+                Aanmelden
+            </h1>
+        </section>
+
+        <section class="ventu-sign-in">
+            <div class="ventu-communication-screen">
+                <div class="ventu-sign-in-container">
+                    <div class="ventu-man-container">
+                        <div class="ventu-man">
+                            <div class="ventu-man-icon ventu-man-icon--account"></div>
+                            <div class="ventu-chat-balloon-container ventu-chat-balloon-left">
+                                <div class="ventu-chat-balloon">
+                                    <div class="ventu-chat-balloon-top"></div>
+                                    <div class="ventu-chat-balloon-middle">
+                                        We zullen jouw gegevens niet delen met externe partijen…
+                                        lorem ipsum dolor sit amet.
                                     </div>
+                                    <div class="ventu-chat-balloon-bottom"></div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="ventu-sign-in-container">
-                    <form>
-                    <div class="ventu-form-section">
-                        <h3>
-                            Meld je aan bij Ventu.nl
-                        </h3>
+            <div class="ventu-sign-in-container">
+                <form>
+                <div class="ventu-form-section">
+                    <h3>
+                        Meld je aan bij Ventu.nl
+                    </h3>
 
-                        <div class="ventu-input-label">
-                            E-mail
-                        </div>
-                        <input type="email" placeholder="Vul een geldig e-mail adres in">
-                        <input type="submit" class="btn orange submit-inactive" value="Ga verder"></input>
+                    <div class="ventu-input-label">
+                        E-mail
                     </div>
-                    </form>
+                    <input type="email" placeholder="Vul een geldig e-mail adres in">
+                    <input type="submit" class="btn orange submit-inactive" value="Ga verder"></input>
                 </div>
-
+                </form>
+            </div>
         </section>
 
         <?php include('php-includes/about.php'); ?>
