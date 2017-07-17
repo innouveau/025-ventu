996cdcac1061cf319529319f7ae8ad4f78381690
Jeroen Breen <jeroen@innouveau.nl>
2017-07-17 17:29:48 +0200

    add icons to popover, fixes #21

--- js/shared/buttons.js
+++ js/shared/buttons.js
@@ -29,7 +29,7 @@ $(window).ready(function(){
         placement: 'top',
         trigger: 'click',
         content: function () {
-            return '<div class="not-in-team"><ul><li><a class="to-teamstack" href="javascript:void(0);" data-team="8E335D59-B7C3-41E3-AF40-33292663B7FB" data-object="7b53a68f-6d0a-4903-87e4-7324bda7d439" data-user="c9ac20fc-88a6-4e52-9513-afbd4310dbcc">Nieuw kantoor voor Ventu.nl</a> </li> <li><a class="to-teamstack" href="javascript:void(0);" data-team="0F8A7755-B5FB-4022-AD80-1816D529A361" data-object="7b53a68f-6d0a-4903-87e4-7324bda7d439" data-user="c9ac20fc-88a6-4e52-9513-afbd4310dbcc">Calcasa verhuist in Delft</a> </li> </ul></div>';
+            return '<div class="not-in-team"><ul><li><a class="to-teamstack" href="javascript:void(0);" data-team="8E335D59-B7C3-41E3-AF40-33292663B7FB" data-object="7b53a68f-6d0a-4903-87e4-7324bda7d439" data-user="c9ac20fc-88a6-4e52-9513-afbd4310dbcc"><span class="ventu-icon ventu-icon--in-text ventu-icon--add"></span>Nieuw kantoor voor Ventu.nl</a> </li> <li><a class="to-teamstack" href="javascript:void(0);" data-team="0F8A7755-B5FB-4022-AD80-1816D529A361" data-object="7b53a68f-6d0a-4903-87e4-7324bda7d439" data-user="c9ac20fc-88a6-4e52-9513-afbd4310dbcc"><span class="ventu-icon ventu-icon--in-text ventu-icon--follow"></span>Calcasa verhuist in Delft</a> </li> </ul></div>';
         }
     });
 });
