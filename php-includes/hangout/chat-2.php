33adb81236d10ce568b0cbee0deab713a42de29e
Jeroen Breen <>
2019-04-24 16:59:44 +0200

    fix hangout chat with new html/css

--- php-includes/hangout/chat-2.php
+++ php-includes/hangout/chat-2.php
@@ -4,62 +4,102 @@
 
     <div class="ventu-popup--hangout-chat ventu-popup" style="display: block;">
         <div class="ventu-popup-body">
-            <img class="ventu-hangout-chat-image" src="http://realspotter.nl/media/objects/22/05/36/ff/220536ff-c1a8-469f-83e0-af6c1a415adc/thumb/1.jpg">
 
-            <div class="ventu-feedback__container ventu-feedback__container--yours ventu-feedback__container--color-1">
-                <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+            <div class="ventu-chat">
+                <div class="ventu-chat__conversation">
+                    <img class="ventu-chat__image" src="http://realspotter.nl/media/objects/22/05/36/ff/220536ff-c1a8-469f-83e0-af6c1a415adc/thumb/1.jpg">
+
+                    <div class="ventu-chat__message ventu-chat__message--yours ventu-chat__message--color-1">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Jan Willem H. de Graaf</p>
+                            <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
+
+                    <div class="ventu-chat__message ventu-chat__message--yours ventu-chat__message--color-2">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Jan Willem H. de Graaf</p>
+                            <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
+
+                     <div class="ventu-chat__message ventu-chat__message--color-2">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Jan Willem H. de Graaf</p>
+                            <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
+
+                    <div class="ventu-chat__message">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Robert</p>
+                            <p class="ventu-member__feedback">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec enim eget ante tristique rhoncus sed eu ante. Nam placerat risus vel tortor </p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
+
+                     <div class="ventu-chat__message">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Robert</p>
+                            <p class="ventu-member__feedback">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec enim eget ante tristique rhoncus sed eu ante. Nam placerat risus vel tortor </p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
+
+                     <div class="ventu-chat__message">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Robert</p>
+                            <p class="ventu-member__feedback">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec enim eget ante tristique rhoncus sed eu ante. Nam placerat risus vel tortor </p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
+
+                     <div class="ventu-chat__message">
+                        <div class="ventu-hangout-member-avatar">
+                            <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
+                        </div>
+                        <div class="ventu-chat__balloon-triangle"></div>
+                        <div class="ventu-chat__balloon">
+                            <p class="ventu-member__name">Robert</p>
+                            <p class="ventu-member__feedback">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec enim eget ante tristique rhoncus sed eu ante. Nam placerat risus vel tortor </p>
+                            <p class="ventu-member__date">14 juni</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="ventu-chat__balloon-triangle"></div>
-                <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
-                    <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
-                    <p class="ventu-member__date">14 juni</p>
+
+                <div class="ventu-chat__input">
+                    <form>
+                        <input id="" name="" placeholder="Schrijf een reactie..." required="" aria-required="true" type="text">
+                        <a class="ventu-chat__submit"></a>
+                    </form>
                 </div>
             </div>
-
-            <div class="ventu-feedback__container ventu-feedback__container--yours ventu-feedback__container--color-2">
-                <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
-                </div>
-                <div class="ventu-chat__balloon-triangle"></div>
-                <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
-                    <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
-                    <p class="ventu-member__date">14 juni</p>
-                </div>
-            </div>
-
-             <div class="ventu-feedback__container ventu-feedback__container--color-2">
-                <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
-                </div>
-                <div class="ventu-chat__balloon-triangle"></div>
-                <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
-                    <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
-                    <p class="ventu-member__date">14 juni</p>
-                </div>
-            </div>
-
-            <div class="ventu-feedback__container">
-                <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/9277f91953bc02aca46ae23754e0c25b?s=60&r=r&d=mm)"> </div>
-                </div>
-                <div class="ventu-chat__balloon-triangle"></div>
-                <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Robert</p>
-                    <p class="ventu-member__feedback">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec enim eget ante tristique rhoncus sed eu ante. Nam placerat risus vel tortor </p>
-                    <p class="ventu-member__date">14 juni</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="ventu-hangout-chat-input">
-            <form class="teamFeedback" novalidate="novalidate">
-                <input id="teamFeedback" name="teamFeedback" placeholder="Schrijf een reactie..." required="" aria-required="true" type="text">
-                <a id="btn-add-stack-feedback"></a>
-            </form>
-        </div>
     </div>
 </div>
