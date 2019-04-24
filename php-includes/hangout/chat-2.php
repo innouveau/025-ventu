39320daab693f1c6e0176f0eb7f41f7b2145fcc2
Jeroen Breen <>
2019-04-24 14:35:57 +0200

    color variations

--- php-includes/hangout/chat-2.php
+++ php-includes/hangout/chat-2.php
@@ -6,7 +6,31 @@
         <div class="ventu-popup-body">
             <img class="ventu-hangout-chat-image" src="http://realspotter.nl/media/objects/22/05/36/ff/220536ff-c1a8-469f-83e0-af6c1a415adc/thumb/1.jpg">
 
-            <div class="ventu-feedback__container ventu-feedback__container--yours">
+            <div class="ventu-feedback__container ventu-feedback__container--yours ventu-feedback__container--color-1">
+                <div class="ventu-hangout-member-avatar">
+                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                </div>
+                <div class="ventu-chat__balloon-triangle"></div>
+                <div class="ventu-chat__balloon">
+                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
+                    <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
+                    <p class="ventu-member__date">14 juni</p>
+                </div>
+            </div>
+
+            <div class="ventu-feedback__container ventu-feedback__container--yours ventu-feedback__container--color-2">
+                <div class="ventu-hangout-member-avatar">
+                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                </div>
+                <div class="ventu-chat__balloon-triangle"></div>
+                <div class="ventu-chat__balloon">
+                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
+                    <p class="ventu-member__feedback">hij lijkt het nu te doen</p>
+                    <p class="ventu-member__date">14 juni</p>
+                </div>
+            </div>
+
+             <div class="ventu-feedback__container ventu-feedback__container--color-2">
                 <div class="ventu-hangout-member-avatar">
                     <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
                 </div>
