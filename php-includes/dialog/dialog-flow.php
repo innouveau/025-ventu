697bfab473e993b85cc481747fddd4458b052f56
Jeroen Breen <>
2019-05-23 12:16:40 +0200

    link out of dialog flow

--- php-includes/dialog/dialog-flow.php
+++ php-includes/dialog/dialog-flow.php
@@ -6,37 +6,38 @@
         <div class="ventu-chat__conversation">
             <div class="ventu-chat__message">
                 <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                    <div class="ventu-avatar-image" style="background-image:url(https://ventu.nl/img/illustrations/ventu-man-100px.png)"> </div>
                 </div>
                 <div class="ventu-chat__balloon-triangle"></div>
                 <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
+                    <p class="ventu-member__name">Ventu assistent</p>
                     <p class="ventu-member__feedback">Hallo kan ik je helpen?</p>
                     <p class="ventu-member__date">20:14</p>
                 </div>
             </div>
 
-            <div class="ventu-chat__message">
+             <div class="ventu-chat__message ventu-chat__message--yours ventu-chat__message--color-1">
                 <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                    <div class="ventu-avatar-image" style="background-image:url(img/icons/hangout/no-avatar-grey.svg)"> </div>
                 </div>
                 <div class="ventu-chat__balloon-triangle"></div>
                 <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
-                    <p class="ventu-member__feedback">... begin maar te typen</p>
-                    <p class="ventu-member__date">20:16</p>
+                    <p class="ventu-member__name">Jij</p>
+                    <p class="ventu-member__feedback">Ik ben op zoek naar een pand in Utrecht</p>
+                    <p class="ventu-member__date">20:17</p>
                 </div>
             </div>
 
-             <div class="ventu-chat__message ventu-chat__message--yours ventu-chat__message--color-1">
+            <div class="ventu-chat__message">
                 <div class="ventu-hangout-member-avatar">
-                    <div class="ventu-avatar-image" style="background-image:url(https://www.gravatar.com/avatar/5455e49389af93f7c4e9ed8905c2c101?s=60&r=r&d=mm)"> </div>
+                    <div class="ventu-avatar-image" style="background-image:url(https://ventu.nl/img/illustrations/ventu-man-100px.png)"> </div>
                 </div>
                 <div class="ventu-chat__balloon-triangle"></div>
                 <div class="ventu-chat__balloon">
-                    <p class="ventu-member__name">Jan Willem H. de Graaf</p>
-                    <p class="ventu-member__feedback">Ik ben op zoek naar een pand in Utrecht</p>
-                    <p class="ventu-member__date">20:17</p>
+                    <p class="ventu-member__name">Ventu assistent</p>
+                    <p class="ventu-member__feedback">Ik heb 95 resultaten voor je gevonden... Wil je huren, kopen voor jezelf of ben je op zoek naar een belegging?<br><br>
+                    Of wil je direct naar de 95 resultaten? <a class="ventu-chat__link">Laat de resultaten zien!</a></p>
+                    <p class="ventu-member__date">20:16</p>
                 </div>
             </div>
         </div>
